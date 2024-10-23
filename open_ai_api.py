b4edff60c432d7d1ebd2f437f4f51d7f08862807
Yoshida Ren <yoshida.ren.r3@dc.tohoku.ac.jp>
2024-10-23 23:18:42 +0900

    chatbotが過去の会話を記憶するよう修正

--- open_ai_api.py
+++ open_ai_api.py
@@ -1,15 +1,32 @@
 #参考：https://note.com/npaka/n/n166bc3df3abc
-from openai import OpenAI
+#from openai import OpenAI
 import config
-client = OpenAI(api_key = config.OPENAI_API_KEY)
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain import PromptTemplate,LLMChain
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.95)
+
+template = """あなたは人間と話すチャットボットです。
+{chat_history}
+Human: {human_input}
+Chatbot:"""
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "human_input"],
+    template=template
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    verbose=True,
+    memory=memory,
+)
 
 #AIからのレスポンスを得る
 def get_response(msg:str) -> str:
-    completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "あなたはかわいらしいアシスタントです。質問に応答してください。"},
-        {"role": "user", "content": f"{msg}"},
-    ]
-    )
-    return completion.choices[0].message.content
+    return llm_chain.run(msg)
